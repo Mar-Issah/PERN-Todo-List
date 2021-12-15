06e064a193507ccc84d1d756d115c55f7a298a1b
Mar-Issah <masy370@gmail.com>
2021-12-15 13:44:31 -0400

    setup express app

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+
+// use port 3000 unless there exists a preconfigured port in env file
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Successfully connected on port ${PORT}`);
+});
