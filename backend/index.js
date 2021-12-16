f33bf7c022027b4233e84b41c23012cb0fc33f26
Mar-Issah <masy370@gmail.com>
2021-12-16 10:18:17 -0400

    completed basic express setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
 // use port 3000 unless there exists a preconfigured port in env file
 const PORT = process.env.PORT || 5000;
+
+// cors middleware. to use middle use app.use
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
 	console.log(`Successfully connected on port ${PORT}`);
