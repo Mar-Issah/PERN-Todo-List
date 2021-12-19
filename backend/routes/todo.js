f32496c61dd3c21229111610f35348be9c0fc907
Mar-Issah <masy370@gmail.com>
2021-12-19 11:22:01 -0400

    update todo route

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -4,7 +4,7 @@ const pool = require("../dbConfig"); //require from the dbConfig file to use to 
 //using express router to create various route for todo
 //---------------------get all todos-------------------------
 router.get("/", async (req, res) => {
-	//this command is to get all the todos
+	//this command is to get all the todos. we dont have to ay returning * because the perpose of select is to return everything
 	try {
 		const allTodo = await pool.query("SELECT * FROM todo");
 		res.status(200).json(allTodo);
@@ -35,36 +35,39 @@ router.post("/add", async (req, res) => {
 	//with rows[0], you will get a whole lot of jibberish in the console or frontend
 });
 
-//-----------------findbyid(id params)----------------------------
-// router.route("/:id").get((req, res) => {
-// 	Exercise.findById(req.params.id)
-// 		.then((exercise) => res.json(exercise))
-// 		.catch((err) => res.status(400).json("Error : " + err));
-// });
+//--------------get a todo (id params)--------------------
+router.get("/:id"),
+	async (req, res) => {
+		//if we go to localhost:5000/todo/random and console.log(req.params) to retrie the route parameter, the response is {id:"random"}
+		try {
+			const { id } = req.params;
+			const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+				id,
+			]);
+			res.status(200).json(todo.rows[0]);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 
+//-----------------update/put-------------------------
+router.put("/update/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { description } = req.body;
+		const updateTodo = await pool.query(
+			"UPDATE todo SET description = $1 WHERE todo_id = $2",
+			[description, id]
+		);
+		res.status(200).json("Todo was updated");
+	} catch (err) {
+		console.log(err.message);
+	}
+});
 //-----------------find and delete-------------------------
 // router.route("/:id").delete((req, res) => {
 // 	Exercise.findByIdAndDelete(req.params.id)
 // 		.then(() => res.json("Exercise deleted"))
 // 		.catch((err) => res.status(400).json("Error: " + err));
 // });
-
-//-----------------update/post-------------------------
-// router.route("/update/:id").post((req, res) => {
-// 	Exercise.findById(req.params.id)
-// 		.then((exercise) => {
-// 			exercise.username = req.body.username;
-// 			exercise.description = req.body.description;
-// 			exercise.duration = Number(req.body.duration);
-// 			exercise.date = Date.parse(req.body.date);
-
-// 			exercise
-// 				.save()
-// 				.then(() => res.json("Exercise updated"))
-// 				.catch((err) => res.status(400).json("Error: " + err));
-// 		})
-
-// 		.catch((err) => res.status(400).json("Error: " + err));
-// });
-
 module.exports = router;
