e4f11e8b4960fceda37ebab77c369d4f04bd5ce4
Mar-Issah <masy370@gmail.com>
2021-12-19 20:17:51 -0400

    installed axios which solved cors error

--- client/src/components/CreateTodo.js
+++ client/src/components/CreateTodo.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import axios from "axios";
 
 const CreateTodo = () => {
 	const [description, setDescription] = useState("");
-	console.log(description);
+	const onSubmitForm = async (e) => {
+		const BASE_URL = "http://localhost:5000";
+		e.preventDefault();
+		axios
+			.post(`${BASE_URL}/todos/add`, description)
+			.then((response) => {
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	return (
 		<div className="p-3">
-			<form className="d-flex mt-3">
+			<form className="d-flex mt-3" onSubmit={onSubmitForm}>
 				<input
 					type="text"
 					className="form-control"
 					placeholder="Type New Todo"
 					value={description}
 					onChange={(e) => setDescription(e.target.value)}
+					autoFocus
+					required
 				/>
 				<Button variant="primary" type="submit">
 					ADD
