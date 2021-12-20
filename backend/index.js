100f601db48683dab0d9797af5dee6c8ff7d295d
Mar-Issah <masy370@gmail.com>
2021-12-20 13:00:02 -0400

    clean up backend code

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,8 @@ require("dotenv").config();
 // use port 3000 unless there exists a preconfigured port in env file
 const PORT = process.env.PORT || 5000;
 
-//----------require routes----------------
-
+//middleware
 app.use(cors());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const pool = require("../dbConfig"); //require from the dbConfig file to use to perform SQL queries here
+const pool = require("../dbConfig");
 
-//using express router to create various route for todo
 //---------------------get all todos-------------------------
 router.get("/", async (req, res) => {
-	//this command is to get all the todos. we dont have to ay returning * because the perpose of select is to return everything
 	try {
 		const allTodo = await pool.query("SELECT * FROM todo");
 		res.status(200).json(allTodo.rows);
@@ -14,16 +12,12 @@ router.get("/", async (req, res) => {
 });
 
 //----------------------add new-------------------------
-//async/await good from asynchronous fxn wait for the fxn  to be completed to continue the fxn
 
-//try/catch in case of error when posting
-
-//request whatever was added to the body
 router.post("/add", async (req, res) => {
 	try {
 		const { description } = req.body;
 		console.log(description);
-		//this is how you use pg.pool https://node-postgres.com/features/queries to write queries whereever you see $1, $@, $3 (placeholder or variable) you fill the values in the array respectively
+
 		const newTodo = await pool.query(
 			"INSERT INTO todo(description) VALUES($1) RETURNING *",
 			[description]
@@ -32,13 +26,11 @@ router.post("/add", async (req, res) => {
 	} catch (err) {
 		console.log(err.message);
 	}
-	//with rows[0], you will get a whole lot of jibberish in the console or frontend
 });
 
 //--------------get a todo (id params)--------------------
 router.get("/:id"),
 	async (req, res) => {
-		//if we go to localhost:5000/todo/random and console.log(req.params) to retrie the route parameter, the response is {id:"random"}
 		try {
 			const { id } = req.params;
 			const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
